f9e640ddae7b6ce550cf3e1b7cd9fee4e650749c
Apipol Sukgler <golf.apipol@gmail.com>
2018-11-24 13:34:44 +0700

    [STEP 1]: start with echo

--- src/main.go
+++ src/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
 
 func main() {
-	fmt.Printf("GO GET TH#3")
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
